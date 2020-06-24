54958602292c3c9f3b1aeff0b712bac45ffe6812
ambassadorSp0ck <aakashbakhle@gmail.com>
2020-06-24 13:22:25 +0530

    Add bit manipulation approaches to Single Number

--- 2206_SingleNumberII.py
+++ 2206_SingleNumberII.py
@@ -13,4 +13,28 @@ class Solution:
                 return k
 
 
-# Bitwise, O(1) space:
+# Bitwise, O(1) space - sum(1)%3 approach
+class Solution:
+    def singleNumber(self, nums):
+        res = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                count += (num >> i) & 1
+            rem = count % 3
+            if i == 31 and rem:
+                res -= 1 << 31
+            else:
+                res |= rem * (1 << i)
+        return res
+
+
+# Bitwise using XOR:
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x1, x0 = 0, 0
+        for num in nums:
+            x1, x0 = x1 ^ (x0 & num), x0 ^ num
+            mask = ~(x1 & x0)
+            x1, x0 = x1 & mask, x0 & mask
+        return x0
