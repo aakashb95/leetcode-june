b98fd64f79a4801245b0203b8092e99806b5464a
ambassadorSp0ck <aakashbakhle@gmail.com>
2020-06-25 16:23:54 +0530

    Leetcode June 22 Count Complete Tree Nodes (222)

---
+++ 2306_CountCompleteTreeNodes.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        # DFS
+        count = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                stack.extend([node.left, node.right])
+                count += 1
+
+        return count
+
+
+# Add optimal approach here
+
