3c2824694be5cca81611e67429289953fb85093f
ambassadorSp0ck <aakashbakhle@gmail.com>
2020-06-25 16:31:27 +0530

    Leetcode June 25 Find the Duplicate Number (287)

---
+++ 2506_FindTheDuplicateNumber.py
@@ -0,0 +1,11 @@
+# Naive, Not allowed:
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
+
+
+# TODO #1 : add cycle Solution
+
